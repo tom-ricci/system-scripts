96d08a48f2d2e4089bf369e69022571ec202fc09
Thomas Ricci <me@thomasricci.dev>
2025-03-16 06:34:25 -0400

    specify commits instead of channel & repo

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "NixOS system management utility";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/cdd2ef009676ac92b715ff26630164bb88fec4e0"; # most recent commit in the nixos-24.11 channel at unix timestamp 1742121143
+    flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b"; # most recent commit in the repo at unix timestamp 1742121143
   };
   outputs = { self, nixpkgs, flake-utils }:
       flake-utils.lib.eachDefaultSystem (system:
